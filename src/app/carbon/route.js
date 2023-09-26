d22df18bd28c7cf67987070410d746d3e534af17
Ramadhan <ramiqcom@gmail.com>
2023-09-26 08:15:21 +0700

    update carbon price

--- src/app/carbon/route.js
+++ src/app/carbon/route.js
@@ -1,5 +1,5 @@
 export async function GET(){
-	const body = await fetch('https://carboncredits.com/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=1700&target_action=get-all-data&default_sorting=old_first&ninja_table_public_nonce=2078b1fd10');
+	const body = await fetch('https://carboncredits.com/wp-admin/admin-ajax.php?action=wp_ajax_ninja_tables_public_action&table_id=1700&target_action=get-all-data&default_sorting=old_first');
 	const object = await body.text();
 	return new Response(object, {
 		status: 200
